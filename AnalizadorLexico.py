17def73f7e3789003582e030b06380620fa5bc00
Josue Ricardo Carias Ordoñez <3661044620101@ingenieria.usac.edu.gt>
2023-10-15 19:16:11 -0600

    se termino el analizado de claves y registros y su tabla correspontiente

--- AnalizadorLexico.py
+++ AnalizadorLexico.py
@@ -1,13 +1,12 @@
 from collections import namedtuple 
-import re
 
 Simbolo= namedtuple("Simbolo",["valor", "linea", "columna"])
 linea=1
 columna=1
 simbolos=[]
-claves=[]
-registros=[]
-final=0
+listaRegistros=[]
+listaClaves=[]
+listaErrores=[]
 
 
 def abrirEntrada(ruta):
@@ -37,35 +36,125 @@ def leerPorSimbolo(ruta):
     return salida
 
 def leerClaves():
-    global final
-    j=0
-    i=0
-    clave=""
-    while str(simbolos[i].valor)!="]":
-        if str(simbolos[i].valor)=="[":
-            j=i
+    i = 0
+    claves = False
+    clave = ""
+    palabraClave = ""
+    sublista = ["C", "l", "a", "v", "e", "s"]  
+    while i < len(simbolos):
+        if all(simbolos[i + j].valor == sublista[j] for j in range(len(sublista))):
+            palabraClave = "Claves"  
+        if palabraClave == "Claves" and simbolos[i].valor == "[":
+            claves = True
+            palabraClave = ""            
+        if claves==True:
+            #caso 1 " "
+            if simbolos[i].valor=='"' and simbolos[i+1].valor=='"':
+                clave+=simbolos[i].valor
+                listaErrores.append("falta una ,: "+ clave)
+                clave=""
+                i+=1
+            #caso 2 clave,"
+            if simbolos[i].valor!='"' and simbolos[i+1].valor==",":
+                clave+=simbolos[i].valor
+                listaErrores.append('''falta una " al final: '''+ clave)
+                clave=""
+                i+=2
+            #caso 3 clave]
+            if simbolos[i].valor!='"' and simbolos[i+1].valor=="]":
+                clave+=simbolos[i].valor
+                listaErrores.append('''falta una " al final: '''+ clave)
+                clave=""
+                break
+            #caso 4 [clave
+            if simbolos[i].valor=='[' and simbolos[i+1].valor!='"':
+                error=""
+                while simbolos[i].valor!='"':
+                    error+=simbolos[i+1].valor                   
+                    i+=1
+                listaErrores.append('''falta una " al inicio: '''+error)           
+                clave = ""
+                i+=2
+            #caso 5 ,clave
+            if simbolos[i].valor==',' and simbolos[i+1].valor!='"':
+                error=""
+                while simbolos[i].valor!='"':
+                    error+=simbolos[i+1].valor                   
+                    i+=1
+                listaErrores.append('''falta una " al inicio: '''+error)           
+                i+=1
+            #para eliminar [ de mas
+            if simbolos[i].valor=='[' and simbolos[i+1].valor=='"':
+                i+=1
+            #mundo perfecto y su final
+            if simbolos[i].valor==',':
+                listaClaves.append(clave)
+                clave=""
+                i+=1
+            if simbolos[i].valor==']':
+                listaClaves.append(clave)
+                clave=""
+            clave += simbolos[i].valor         
+        if palabraClave == "" and simbolos[i].valor == "]":
+            claves = False        
         i+=1
-    for pos in range(j+1,i):
-        clave+=(simbolos[pos].valor)
-    claves.extend(re.findall(r'"(.*?)"', clave))
-    final=i
-    return(claves)
+    for pos in range(len(listaClaves)):
+        print(listaClaves[pos])
+    for pos in range(len(listaErrores)):
+        print(listaErrores[pos])
+        
+    return listaClaves
 
 def leerRegistros():
-    global final
-    final+=12
-    registro=""
-    print("-----------------------------")  
-    for pos in range(final,len(simbolos)-1):
-        registro+=simbolos[pos].valor
-        if(str(simbolos[pos].valor)=="}"):
-            registro+="\n"
-    print(registro)
-    return(registros)
+    i = 0
+    registros = False
+    registro = ""
+    palabraregistro = ""
+    sublista = ["r", "e", "g", "i", "s", "t", "r", "o", "s"]  
+    while i < len(simbolos):
+        if all(simbolos[i + j].valor == sublista[j] for j in range(len(sublista))):
+            palabraregistro = "registros"  
+        if palabraregistro == "registros" and simbolos[i].valor == "[":
+            registros = True
+            palabraregistro = "" 
+        if registros==True:
+            if simbolos[i].valor == '{':
+                i+=1
+            if simbolos[i].valor == '[':
+                i+=2
+            if simbolos[i].valor == '}':
+                i+=1
+            if simbolos[i].valor == ',':
+                listaRegistros.append(registro)
+                registro = "" 
+            else:
+                registro += simbolos[i].valor                
+                  
+        if palabraregistro == "" and simbolos[i].valor == "]":
+            listaRegistros.append(registro[:-1])
+            registros = False      
+        i += 1  
 
-  
+    listaRegistros.pop(0)
+    #for elemento in range(len(listaRegistros)):
+    #    print(str(elemento+1)+" "+listaRegistros[elemento])
 
-print(abrirEntrada("entrada1.txt"))
-leerPorSimbolo("entrada1.txt")
-print(leerClaves())
-print(leerRegistros())
+    
+
+    return listaRegistros
+
+    
+
+print("--------------------------------")
+(abrirEntrada("entrada1.txt"))
+print("--------------------------------")
+(leerPorSimbolo("entrada1.txt"))
+print("--------------------------------")
+(leerClaves())
+print("--------------------------------")
+(leerRegistros())
+
+
+
+
+
--- Tablas.py
+++ Tablas.py
@@ -8,12 +8,24 @@ def reporteHtml():
 
     tabla_html = '''<<TABLE BORDER="1" CELLBORDER="1" CELLSPACING="0">'''
     tabla_html +='''<TR><TD>Reporte HTML de abarrotería 1</TD></TR>'''
+    
     tabla_html +='''<TR>'''
-
-    for pos in range(len(claves)):
-        tabla_html += "<TD>"+str(claves[pos])+ "</TD>"
-
+    for pos in range(len(listaClaves)):
+        tabla_html += "<TD>"+str(listaClaves[pos])+ "</TD>"
     tabla_html +='''</TR>'''
+
+    numeroDeFilas=int(((len(listaRegistros))/(len(listaClaves))))
+    numeroDeColumnas=int((len(listaClaves)))
+    
+    print("Filas:"+str(numeroDeFilas))
+    print("Columnas:"+str(numeroDeColumnas))
+
+    for filas in range(numeroDeFilas):
+        tabla_html +='''<TR>'''
+        for columna in range(numeroDeColumnas):
+            tabla_html += "<TD>"+str(listaRegistros[(filas*numeroDeColumnas)+columna])+ "</TD>"
+        tabla_html +='''</TR>'''
+
     tabla_html +='''</TABLE>>'''
 
     dot.node('tabla', label=tabla_html, shape='none')
